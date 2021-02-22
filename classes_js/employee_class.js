4283bbae3181d2a82476ce04d6fb91d4efa5f992
wryanj <ryanjohnson9685@gmail.com>
2021-02-21 20:01:35 -0500

    updating first working html template for reference in writing html. storing as template in rendered_files_html

--- classes_js/employee_class.js
+++ classes_js/employee_class.js
@@ -14,22 +14,22 @@
 
         // Define Class Methods
 
-            // Get name method that takes a name as an argument, and sets it as the name for this class instance
+            // Method to return employee name
             getName (name) {
                 return this.name
             }
 
-            // Get id method that takes an employeeID as an argument, and sets it to the employeeID for this class instance
+            // Method to return employee ID
             getID (employeeID) {
                 return this.employeeID
             }
 
-            
-            // Get id method that takes an email as an argument, and sets it to the email for this class instance
+            // Method to return employee email
             getID (email) {
                 return this.email 
             }
 
+            // Method to return employee role
             getRole() {
                 return "Employee"
             }
